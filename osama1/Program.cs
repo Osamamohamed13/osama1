96573e131b7ff994fa4e8e25749b8157a67e0ed9
osama eng <osama.eng_5802@eng.kfs.edu.eg>
2021-11-27 12:23:19 +0200

    Add project files.

---
+++ osama1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ReversedLinkedList
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> fantasy = new LinkedList<string>();
+
+            LinkedListNode<string> teams = fantasy.AddFirst("Arsenal");
+
+            fantasy.AddLast("Man united");
+            fantasy.AddAfter(teams, "Totenham");
+            fantasy.AddBefore(teams, "Man city");
+            fantasy.AddLast("Liverpool");
+
+            Console.WriteLine("The linked List Before Reversing: \n ");
+
+            foreach (var item in fantasy)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            Console.WriteLine("The linked List After Reversing: \n ");
+
+            foreach (var item in fantasy.Reverse())
+            {
+
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            Console.ReadKey();
+        }
+
+    }
+}
